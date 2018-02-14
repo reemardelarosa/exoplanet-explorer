4a8cc4e8660ca492372125171f39c7e3258406fc
delaj161 <john.x.dela.-nd@disney.com>
2018-02-14 09:54:30 +0800

    Solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,11 +26,15 @@ proper order even if all the requests haven't finished.
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name);
+      resolve();
+    });
   }
 
   /**
@@ -48,8 +52,20 @@ proper order even if all the requests haven't finished.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
+    console.log('sent: ' + url);
     return get(url).then(function(response) {
-      return response.json();
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url);
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url);
+        return response.json();
+      }
     });
   }
 
@@ -58,6 +74,35 @@ proper order even if all the requests haven't finished.
     /*
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      // .then(function(response) {
+      //   addSearchHeader(response.query);
+
+      //   Promise.all(response.results.map(getJSON))
+      //     .then(function(planets) {
+      //       var seq = Promise.resolve();
+
+      //       planets.forEach(function(planet) {
+      //         seq = seq.then(function() {
+      //           createPlanetThumb(planet);
+      //         });
+      //       });
+      //     });
+      // });
+      .then(function(response) {
+        addSearchHeader(response.query);
+        return response;
+      })
+      .then(function(response) {
+        var seq = Promise.resolve();
+
+        var arrayOfPromises = response.results.map(getJSON);
+
+        arrayOfPromises.forEach(function(promise)  {
+          seq = seq.then(function() {
+            return promise.then(createPlanetThumb);
+          });
+        });
+      });
   });
 })(document);
